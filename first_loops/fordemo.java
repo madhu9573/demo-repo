8fe88eb23d433498cab68952b0bc234d66084596
madhu9573 <madhujavabu@gmail.com>
2025-10-10 23:37:51 +0530

    for loop

---
+++ first_loops/fordemo.java
@@ -0,0 +1,11 @@
+package first_loops;
+
+public class fordemo {
+public static void main(String[] args) {
+   //for loop
+   for(int i=10;i>=1;i--){
+    System.out.println("Madhu Sudan");
+   }
+   } 
+}
+
